67337d251434060a540f9829bc35a6d4eaaef899
Sam Farley <samanthafarley32@gmail.com>
2021-09-25 18:23:37 -0400

    random word generation complete

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let e_word = document.getElementById("word");
 
-let e_words = ["eat", "happy", "butterfly"];
-let s_words = ["comer", "feliz", "marisposa"];
+let e_words = ["eat", "happy", "butterfly", "family", "brother", "sister"];
+let s_words = ["comer", "feliz", "marisposa", "familia", "hermano", "hermana"];
 
 let rand = Math.floor(Math.random()*e_words.length);
 e_word.innerHTML = e_words[rand];
@@ -39,6 +39,53 @@ platforms.push({x: 900, y: 200, w: 100, h: 10, color: "#f1bf00"});
 platforms.push({x: 450, y: 550, w: 150, h: 10, color: "#f1bf00"});
 platforms.push({x: 600, y: 300, w: 250, h: 10, color: "#aa151b"});
 platforms.push({x: 200, y: 350, w: 100, h: 10, color: "#aa151b"});
+
+/*** Random spanish word generation ***/
+let collided = false;
+let mult_choice = [];
+let chosen_plats = [];
+// 3 random spanish words (index)
+let option1 = Math.floor(Math.random()*s_words.length);
+let option2 = Math.floor(Math.random()*s_words.length);
+let option3 = Math.floor(Math.random()*s_words.length);
+// 3 random platforms (index)
+let p1 = Math.floor(Math.random()*platforms.length);
+let p2 = Math.floor(Math.random()*platforms.length);
+let p3 = Math.floor(Math.random()*platforms.length);
+
+// remove duplicate spanish words
+// while (option1 == option2 || option1 == option3 || option2 == option3) {
+//     if (option1 == option2) {
+//         option2 = Math.floor(Math.random()*s_words.length);
+//     } else if (option1 == option3) {
+//         option3 = Math.floor(Math.random()*s_words.length);
+//     } else { //option2 = option3
+//         option3 = Math.floor(Math.random()*s_words.length);
+//     }
+// }
+mult_choice.push(rand);
+mult_choice.push(option1);
+mult_choice.push(option2);
+mult_choice.push(option3);
+mult_choice = [...new Set(mult_choice)];
+// remove duplicate platforms
+// while (p1 == p2 || p1 == p3 || p2 == p3) {
+//     if (p1 == p2) {
+//         let p2 = Math.floor(Math.random()*platforms.length);
+//     } else if (p1 == p3) {
+//         let p3 = Math.floor(Math.random()*platforms.length);
+//     } else { // p2 = p3
+//         let p3 = Math.floor(Math.random()*platforms.length);
+//     }
+// }
+chosen_plats.push(p1);
+chosen_plats.push(p2);
+chosen_plats.push(p3);
+chosen_plats = [...new Set(chosen_plats)];
+
+while (mult_choice.length < chosen_plats.length) {
+    chosen_plats.length--;
+}
 
 document.addEventListener("keydown", (e) => {
     e.preventDefault();
@@ -82,48 +129,10 @@ function platform() {
 }
 
 function placing_words() {
-    // 3 random spanish words (index)
-    // let option1 = Math.floor(Math.random()*s_words.length);
-    // let option2 = Math.floor(Math.random()*s_words.length);
-    // let option3 = Math.floor(Math.random()*s_words.length);
-    // 3 random platforms (index)
-    let p1 = Math.floor(Math.random()*platforms.length);
-    let p2 = Math.floor(Math.random()*platforms.length);
-    let p3 = Math.floor(Math.random()*platforms.length);
-
-    //let mult_choice = [];
-    let chosen_plats = [];
-    // remove duplicate spanish words
-    // while (option1 == option2 || option1 == option3 || option2 == option3) {
-    //     if (option1 == option2) {
-    //         option2 = Math.floor(Math.random()*s_words.length);
-    //     } else if (option1 == option3) {
-    //         option3 = Math.floor(Math.random()*s_words.length);
-    //     } else { //option2 = option3
-    //         option3 = Math.floor(Math.random()*s_words.length);
-    //     }
-    // }
-    // mult_choice.push(option1);
-    // mult_choice.push(option2);
-    // mult_choice.push(option3);
-    // remove duplicate platforms
-    while (p1 == p2 || p1 == p3 || p2 == p3) {
-        if (p1 == p2) {
-            let p2 = Math.floor(Math.random()*platforms.length);
-        } else if (p1 == p3) {
-            let p3 = Math.floor(Math.random()*platforms.length);
-        } else { // p2 = p3
-            let p3 = Math.floor(Math.random()*platforms.length);
-        }
-    }
-    chosen_plats.push(p1);
-    chosen_plats.push(p2);
-    chosen_plats.push(p3);
-
     for (let i = 0; i < chosen_plats.length; i++) {
-        cx.fillStyle = platforms[chosen_plats[i]];
+        cx.fillStyle = "#000000";
         cx.font = "50px Arial";
-        cx.fillText(s_words[i], platforms[chosen_plats[i]].x, platforms[chosen_plats[i]].y - 30);
+        cx.fillText(s_words[mult_choice[i]], platforms[chosen_plats[i]].x, platforms[chosen_plats[i]].y - 30);
     }
 }
 
@@ -135,7 +144,9 @@ function animate() {
     y += ysp + pull;
     move();
     platform();
-    placing_words();
+    if (!collided) {
+        placing_words();
+    }
 }
 
 animate();
